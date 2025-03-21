d45f585c8f68022c24cd5d1588a9bbea1635948c
Ansar Keles <ansar.keles@Ansars-MacBook-Air.local>
2025-03-21 23:04:45 +0500

    election day

--- main.go
+++ main.go
@@ -9,23 +9,7 @@ import (
 var candidates = []string{"Ваня", "Рустам", "Куаныш", "Нурислам"}
 
 func main() {
-	/*
-		По аналогии функции `getZhambylOblVotes` реализовать подсчет голосов для ТРЕХ областей.
-		Использовать каналы для синхронизации исполнения горутин и получения результов подсчета голосов.
-		Реализовать функцию `calcVotes` для финального подсчета голосов всех областей.
 
-		Можно использовать код сниппеты ниже для вашего удобства.
-
-		====================
-		go getZhambylOblVotes(...)
-
-		results := calcVotes(...)
-		for name, numOfVotes := range results {
-			fmt.Printf("Кандидат [%s] набрал [%d] голосов\n", name, numOfVotes)
-		}
-	*/
-
-	// ВАШ КОД ТУТ...
 	zhambylCh := make(chan map[string]int)
 	almatyCh := make(chan map[string]int)
 	akmolaCh := make(chan map[string]int)
@@ -60,16 +44,12 @@ func getZhambylOblVotes(ch chan<- map[string]int) {
 	votes := make(map[string]int)
 	maxNumOfVotes := 100
 
-	// Подсчитываем голоса кандидатов.
-	// Каждому кандидату добавляем случайное количество голосов в диапазоне [0, maxNumOfVotes)
 	for _, name := range candidates {
 		votes[name] = rand.Intn(maxNumOfVotes)
 	}
 
 	time.Sleep(1 * time.Second)
 
-	// Пишем результат подсчета голосов в канал
-	// ВАШ КОД ТУТ...
 	ch <- votes
 
 	fmt.Println("Подсчет голосов в Жамбылской области завершен")
